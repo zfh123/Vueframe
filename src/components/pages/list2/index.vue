40d2ae55e5f4869ec8ea4fc1e812d039fe4c6dab
zfh123 <1435925296@qq.com>
2017-08-20 02:53:29 +0800

    ＶＵＥ项目结构

---
+++ src/components/pages/list2/index.vue
@@ -0,0 +1,29 @@
+<style>
+
+</style>
+<template>
+  <div class="list2">
+      list2
+  </div>
+</template>
+<script>
+import { test1, test2 } from 'api/list1'
+export default {
+    data () {
+        return {
+
+        }
+    },
+    created () {
+        this._test2()
+    },
+    mounted () {
+
+    },
+    methods: {
+        _test2 () {
+            test1('list2')
+        }
+    }
+}
+</script>
---
+++ src/components/pages/my/index.vue
@@ -0,0 +1,29 @@
+<style>
+
+</style>
+<template>
+  <div class="my">
+      my
+  </div>
+</template>
+<script>
+import { test1, test2 } from 'api/list1'
+export default {
+    data () {
+        return {
+
+        }
+    },
+    created () {
+        this._test3()
+    },
+    mounted () {
+
+    },
+    methods: {
+        _test3 () {
+            test1('my')
+        }
+    }
+}
+</script>
